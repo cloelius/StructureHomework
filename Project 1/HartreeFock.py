ada6ca85824d8b858bdda3ed06fae13ae79c23d4
cloelius <cloelius1@gmail.com>
2014-03-17 14:46:07 -0400

    More update

--- Project 1/HartreeFock.py
+++ Project 1/HartreeFock.py
@@ -1,4 +1,133 @@
+from itertools import product
+from itertools import combinations as cwr
 from MatrixElCalc import *
-for it in range(4):
+import numpy as np
+states=[]
+numparts=2
+for it in range(numparts):
     states.append(State([QuantumNumber("p",it),QuantumNumber("sigma",'+')]))
     states.append(State([QuantumNumber("p",it),QuantumNumber("sigma",'-')]))
+H0=TotalHamiltonian([])
+H1=TotalHamiltonian([])
+H2=TotalHamiltonian([])
+epsilon=1
+V=2
+W=1
+for state in states:
+    if state.GetVal("sigma") =='+':
+        H0.Add(Hamiltonian(epsilon/2.0,[state.anop,state.creop]))
+    if state.GetVal("sigma") =='-':
+        H0.Add(Hamiltonian(-epsilon/2.0,[state.anop,state.creop]))
+        
+for combo in product(['+','-'],list(range(numparts)),list(range(numparts))):
+    ham=[0,1,2,3]
+    for state in states:
+        if state.GetVal("sigma")!=combo[0]:
+            if state.GetVal("p")==combo[1]:
+                ham[0]=state.anop   
+    for state in states:
+        if state.GetVal("sigma")!=combo[0]:
+            if state.GetVal("p")==combo[2]:
+                ham[1]=state.anop
+    for state in states:
+        if state.GetVal("sigma")==combo[0]:
+            if state.GetVal("p")==combo[2]:
+                ham[2]=state.creop
+    for state in states:
+        if state.GetVal("sigma")==combo[0]:
+            if state.GetVal("p")==combo[1]:
+                ham[3]=state.creop
+    H1.Add(Hamiltonian(V/2.0,ham))
+    
+for combo in product(['+','-'],list(range(numparts)),list(range(numparts))):
+    ham=[0,1,2,3]
+    for state in states:
+        if state.GetVal("sigma")!=combo[0]:
+            if state.GetVal("p")==combo[1]:
+                ham[0]=state.anop   
+    for state in states:
+        if state.GetVal("sigma")==combo[0]:
+            if state.GetVal("p")==combo[2]:
+                ham[1]=state.anop
+    for state in states:
+        if state.GetVal("sigma")!=combo[0]:
+            if state.GetVal("p")==combo[2]:
+                ham[2]=state.creop
+    for state in states:
+        if state.GetVal("sigma")==combo[0]:
+            if state.GetVal("p")==combo[1]:
+                ham[3]=state.creop
+    H2.Add(Hamiltonian(W/2.0,ham))
+
+Htot=TotalHamiltonian([])
+#for Ham in H0.Hamiltonians:
+ #   Htot.Add(Ham)
+for Ham in H1.Hamiltonians:
+    Htot.Add(Ham)
+for Ham in H2.Hamiltonians:
+    Htot.Add(Ham)
+    
+vec=StateVec()
+class HamilMat:
+    kets=[]
+    states=[]
+    H=[]
+    interactions=0
+    mat=[]
+    def __init__(self,Hamil,states,i):
+        self.states=states
+        self.interactions=i
+        if type(states[0]) is StateVec:
+            self.H=Hamil
+            self.kets=states
+        else:
+            self.kets=[]
+            self.H=Hamil
+           # for i in range(len(states)):
+            for state in (cwr(states,i)):
+                    print(state)
+                    self.kets.append(StateVec(list(state)))
+           # for state in self.kets:
+            #    print(state)
+             #   self.kets.append(list(state))
+              #  self.kets.remove(state)
+        
+            
+    def MakeMatrix(self):
+        #states=self.kets
+        states=self.states
+        Htot=self.H
+        mat=np.zeros([len(states),len(states),len(states),len(states)])
+        
+        
+        
+        
+        
+        
+        
+        
+        for i,state in enumerate(states):
+            for j,state1 in enumerate(states):
+                #vec1=StateVec([state,state1])
+                for state2 in states:
+                    mat[i][j]=GetMatrixElement(Htot,state,state1)
+            self.mat=mat
+              #  if (i>10):
+               #     return 0
+                 #   for state3 in states:
+                #        vec2=StateVec([state2,state3])
+               #         mat[i][j]=(GetMatrixElement(Htot,vec1,vec2))
+              #          del vec2
+                     
+             #   del vec1
+            #return mat
+
+EnergyMat=HamilMat(H0,states,1)
+EnergyMat.MakeMatrix()
+Matrix=HamilMat(Htot,states,2)
+Matrix.MakeMatrix()
+#for i in range(len(Matrix.mat)):
+ #   Matrix.mat[i][i]+=EnergyMat.mat[i][i]
+C=np.eye(len(states))
+
+
