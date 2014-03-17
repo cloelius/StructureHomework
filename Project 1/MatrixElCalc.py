3f5b96e19777ece029776a9c3b3884825cf79e45
cloelius <cloelius1@gmail.com>
2014-03-17 18:13:32 -0400

    Upda

--- Project 1/MatrixElCalc.py
+++ Project 1/MatrixElCalc.py
@@ -1,29 +1,51 @@
-import copy
+import itertools
 class StateVec:
     State=[]
     const=0
+    def copy(self):
+        
+        copystate=[]
+       # print (self.State)
+        try:
+            for s in self.State:
+                copystate.append(s.copy())
+        except:
+                copystate=0
+        return StateVec(copystate,self.const)
     def __init__(self,State=[],cons=1):
         self.State=State
         self.const=cons
+        if type(State) is  list:
+            for state in itertools.combinations(self.State,2):
+                if state[0].Compare(state[1]):
+                    self.State=0
+            
+                
+    
     def dot(self,vec):
         try:
             w=0
-          #  print(len(vec))
+            #print(len(vec))
             for x in vec:
                 w=w+self.dot(x)
-           #     print(w)
+             #   print(w)
             return w
         except:
             if self.State==0 or vec.State ==0:
+                #print('hi')
                 return 0
             if len(self.State) != len(vec.State):
+                #print('hi')
                 return 0
             for x in self.State:
                 same=False
                 for y in vec.State:
                     if x.Compare(y):
+                #        return 0
+                 #       print(same)
                         same=True
                 if not same:
+                  #  print('bye')
                     return 0
             return self.const*vec.const
         
@@ -34,7 +56,8 @@ class QuantumNumber:
     def __init__(self,name,value):
         self.name=name
         self.value=value
-
+    def copy(self):
+        return QuantumNumber(self.name,self.value)
 class Hamiltonian:
     opers=[]
     const=0
@@ -42,15 +65,22 @@ class Hamiltonian:
           self.opers=opers
           self.const=const
     def Act(self,vec):
-        opers=copy.deepcopy(self.opers)
+        #print(self.opers)
+        opers=[]
+        for item in self.opers:
+            opers.append(item)
         try:
             vecs=[]
+          #  print(vec)
+           # print(2)
             for x in vec:
                 vecs.append(self.Act(x))
             return vecs
         except:
+          #  print(vec)
+           # print(1)
             if len(opers)==0:
-                vec1=copy.deepcopy(vec)
+                vec1=vec.copy()
                 vec1.const=self.const
                 return vec1
             update=Hamiltonian(self.const,opers[1:])
@@ -75,8 +105,11 @@ class Operator:
     def __init__(self,state,Creation):
         self.state=state
         self.Creation=Creation
+    def copy(self):
+        return Operator(self.state.copy(),self.Creation)
     def Mult(self,statevecorig):
-        statevec=copy.deepcopy(statevecorig)
+        statevec=statevecorig.copy()
+        #statevec=StateVec(State(statevecorig.quatumnums),statevecorig.const)
         if statevec.State == 0:
             return statevec
         
@@ -105,13 +138,19 @@ class State:
     quantumnums=[]
     anop=0
     creop=0
+    def copy(self):
+        qcopy=[]
+        for q in self.quantumnums:
+          #  print(q)
+            qcopy.append(q.copy())
+        return State(qcopy)
     def __init__(self,quantumnums):
         self.quantumnums=quantumnums
         self.creop=Operator(self,True)
         self.anop=Operator(self,False)
     def Compare(self,state):
         if len(self.quantumnums) != len(state.quantumnums):
-            return false
+            return False
         for num in self.quantumnums:
             exist=False
             for num1 in state.quantumnums:
@@ -122,7 +161,14 @@ class State:
             if(not exist):
                 return False
         return True 
+    def GetVal(self,name):
+        for q in self.quantumnums:
+            if q.name==name:
+                return q.value
+
+    
+    
 def GetMatrixElement(Ham,bra,ket):
-    return bra.dot(Ham.Act(ket))
+        return bra.dot(Ham.Act(ket))
 
 
